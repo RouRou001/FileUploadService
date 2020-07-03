a0463b9bf22885fec938a6ad8dd0e3ef67c1321e
RuneYap <runelaboratory94@gmail.com>
2020-07-04 01:06:28 +0800

    fix CORS allow origin

--- Controllers/UploadFileController.cs
+++ Controllers/UploadFileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,18 +14,25 @@ namespace FileUploadService.Controllers
     public class FileUploadController : ControllerBase
     {
 
+        [DisableCors]
         [HttpPost]
+        [EnableCors("AllowOrigin")]
         public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
         {
             System.Console.WriteLine("DEBUG XXX : OnPostUploadAsync called");
+            System.Console.WriteLine("DEBUG XXX count : " + files.Count);
             long size = files.Sum(f => f.Length);
 
+            System.Console.WriteLine("DEBUG XXX size : " + size);
             foreach (var formFile in files)
             {
+                System.Console.WriteLine("DEBUG XXX : formFileLength " + formFile.Length);
                 if (formFile.Length > 0)
                 {
                     var filePath = Path.Combine("StoredFiles",
                         Path.GetRandomFileName());
+
+                    System.Console.WriteLine("DEBUG XXX filePath : " + filePath);
 
                     using (var stream = System.IO.File.Create(filePath))
                     {
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,7 +9,7 @@ namespace FileUploadService
 {
     public class Startup
     {
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        //readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         public Startup(IConfiguration configuration)
         {
@@ -27,12 +21,8 @@ namespace FileUploadService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options => {
-                options.AddPolicy(name: MyAllowSpecificOrigins, builder => {
-                    builder.WithOrigins("http://localhost:8100", "https://localhost:5001")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
+            services.AddCors(c => {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
             });
 
             services.AddControllers();
@@ -50,7 +40,7 @@ namespace FileUploadService
 
             app.UseRouting();
 
-            app.UseCors();
+            app.UseCors(options => options.WithOrigins("https://localhost:5001"));
 
             app.UseAuthorization();
 
